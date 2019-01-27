403b0a618153ba2c5184ddb72e281639ea7bfd6c
Nazmi Febrian <nazmi.febrian@gmail.com>
2019-01-27 17:41:29 +0700

    Add publish temp and humid to mosquitto server in Ubuntu 16.0 PC

--- Wemos_DHT11_MQTT.ino
+++ Wemos_DHT11_MQTT.ino
@@ -2,13 +2,25 @@
 #include <PubSubClient.h>
 #include <ESP8266WiFi.h>
 
-#define wifi_ssid "Mako Brimob"
-#define wifi_password "G0beyondexpectation!"
+#define wifi_ssid "SSID"
+#define wifi_password "Password"
 
-#define DHTPIN D4
+#define mqtt_server "IP_Mosquitto"
+#define mqtt_user "user"
+#define mqtt_password "pass"
+
+#define temperature_topic "temperature"
+#define humidity_topic "humidity"
+
+// Buffer to decode MQTT messages
+char message_buff[100];
+
+long lastMsg = 0;   
+long lastRecu = 0;
+bool debug = true;
+
+#define DHTPIN 4
 #define DHTTYPE DHT11
-
-bool debug = false;
 
 DHT dht(DHTPIN, DHTTYPE);
 WiFiClient espClient;
@@ -18,6 +30,8 @@ void setup() {
   Serial.begin(9600);
   pinMode(D2, OUTPUT);
   setup_wifi();
+  client.setServer(mqtt_server, 1883);
+//  client.setCallback(callback);
   dht.begin();
 }
 
@@ -44,16 +58,44 @@ void setup_wifi(void)
   Serial.print(WiFi.localIP());
 }
 
-void loop() {
-  // Read humidity
-  float h = dht.readHumidity();
-  // Read temperature in Celcius
-  float t = dht.readTemperature();
+void reconnect() {
 
-  if (debug) {
-    Serial.print("Temperature : ");
-    Serial.print(t);
-    Serial.print(" | Humidity : ");
-    Serial.println(h);
+  while (!client.connected()) {
+    Serial.print("Connecting to MQTT broker ...");
+    if (client.connect("ESP8266Client", mqtt_user, mqtt_password)) {
+      Serial.println("OK");
+    } else {
+      Serial.print("KO, error : ");
+      Serial.print(client.state());
+      Serial.println(" Wait 5 secondes before to retry");
+      delay(5000);
+    }
+  }
+}
+
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  long now = millis();
+  // Send a message every second
+  if(now -  lastMsg > 1000 * 1){
+    lastMsg = now;
+    // Read humidity
+    float h = dht.readHumidity();
+    // Read temperature in Celcius
+    float t = dht.readTemperature();
+  
+    if (debug) {
+      Serial.print("Temperature : ");
+      Serial.print(t);
+      Serial.print(" | Humidity : ");
+      Serial.println(h);
+    }
+
+    client.publish(temperature_topic, String(t).c_str(), true);
+    client.publish(humidity_topic, String(h).c_str(), true);
   }  
 }
